4b47298df2fa39ec898f4090a19157ee81d72a8a
UnnamedOrange <lycheng1215@sina.com>
2022-04-09 01:13:59 +0800

    Create empty project

---
+++ tomato-clock-ex/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#include "mbed.h"
+
+int main()
+{
+    while (true)
+    {
+    }
+}
