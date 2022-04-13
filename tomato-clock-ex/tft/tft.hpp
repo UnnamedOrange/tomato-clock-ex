5bbd31568c0164ed7008b14ae454ea36b2929f84
UnnamedOrange <lycheng1215@sina.com>
2022-04-13 14:01:33 +0800

    Implement tft_ll_device

---
+++ tomato-clock-ex/tft/tft.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file tft.hpp
+ * @author UnnamedOrange
+ * @brief Include headers of TFT library.
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#pragma once
+
+#include "tft_ll_device.hpp"
